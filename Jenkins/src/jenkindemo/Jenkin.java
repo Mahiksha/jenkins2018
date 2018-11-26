628ff38e5deaa2b503c3fc75f8c25495973929d3
Apeksha <Apeksha@192.168.0.15>
2018-11-26 16:09:22 -0500

    facebook alotsof

--- Jenkins/src/jenkindemo/Jenkin.java
+++ Jenkins/src/jenkindemo/Jenkin.java
@@ -14,7 +14,7 @@ public class Jenkin {
 		System.setProperty("Webdriver.chrome.driver","chromedriver.exe");
 		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 		driver.get("https://www.facebook.com/");
-		System.out.println("I love facebook alots");
+		System.out.println("I love facebook alotsof");
 		driver.close();
 		
 		
