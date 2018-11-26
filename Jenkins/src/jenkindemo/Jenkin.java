ca2e7f5775f3ce9756ed6560fdd6fc97c9cd61c3
Apeksha <Apeksha@192.168.0.15>
2018-11-25 19:30:24 -0500

    for building in jenkins

---
+++ Jenkins/src/jenkindemo/Jenkin.java
@@ -0,0 +1,27 @@
+package jenkindemo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Jenkin {
+	
+	public static void main(String[] args){
+		WebDriver driver=new ChromeDriver();
+		System.setProperty("Webdriver.chrome.driver","chromedriver.exe");
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.get("https://www.facebook.com/");
+		System.out.println("facebook");
+		driver.close();
+		
+		
+			
+		
+			
+		
+	}
+
+}
